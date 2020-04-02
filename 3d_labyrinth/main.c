34074f3744cf29394e8273054880cef57518e334
maure <romain.maure@epfl.ch>
2020-04-02 19:41:07 +0200

    modified : main , imu qui affiche bien les leds

--- 3d_labyrinth/main.c
+++ 3d_labyrinth/main.c
@@ -6,18 +6,40 @@
 #include "ch.h"
 #include "chprintf.h"
 #include "hal.h"
-#include "memory_protection.h"
-#include <main.h>
+#include "shell.h"
 
+#include "aseba_vm/aseba_node.h"
+#include "aseba_vm/skel_user.h"
+#include "aseba_vm/aseba_can_interface.h"
+#include "aseba_vm/aseba_bridge.h"
+#include "audio/audio_thread.h"
+#include "audio/play_melody.h"
+#include "audio/play_sound_file.h"
+#include "audio/microphone.h"
+#include "camera/po8030.h"
+#include "epuck1x/Asercom.h"
+#include "epuck1x/Asercom2.h"
+#include "epuck1x/a_d/advance_ad_scan/e_acc.h"
+#include "sensors/battery_level.h"
 #include "sensors/imu.h"
 #include "sensors/mpu9250.h"
+#include "sensors/proximity.h"
+#include "sensors/VL53L0X/VL53L0X.h"
+#include "cmd.h"
+#include "config_flash_storage.h"
+#include "exti.h"
 #include "i2c_bus.h"
-#include "epuck1x/a_d/advance_ad_scan/e_acc.h" // fonction angle
-#include "usbcfg.h"
-
+#include "ir_remote.h"
 #include "leds.h"
+#include <main.h>
+#include "memory_protection.h"
 #include "motors.h"
-
+#include "sdio.h"
+#include "selector.h"
+#include "spi_comm.h"
+#include "usbcfg.h"
+#include "communication.h"
+#include "uc_usage.h"
 
 #define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
 
@@ -25,7 +47,16 @@ messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
+parameter_namespace_t parameter_root, aseba_ns;
 
+static THD_WORKING_AREA(selector_thd_wa, 2048);
+
+static bool load_config(void)
+{
+    extern uint32_t _config_start;
+
+    return config_load(&parameter_root, &_config_start);
+}
 
 static void serial_start(void)
 {
@@ -39,35 +70,24 @@ static void serial_start(void)
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-
-
-static THD_WORKING_AREA(imu_thd_wa, 2048);
-
-static THD_FUNCTION(imu_thd, arg)
+static THD_FUNCTION(selector_thd, arg)
 {
     (void) arg;
     chRegSetThreadName(__FUNCTION__);
 
-    //int16_t leftSpeed = 0, rightSpeed = 0;
 
     messagebus_topic_t *imu_topic = messagebus_find_topic_blocking(&bus, "/imu");
     imu_msg_t imu_values;
 
+    calibrate_acc();
+    calibrate_gyro();
+
     while(1)
     {
-    	calibrate_acc();
-		calibrate_gyro(); /////////////////// a voir si nécessaire
 
-		//wait for new measures to be published
-		messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
-
-		// Read IMU.
-		chprintf((BaseSequentialStream *)&SDU1, "IMU\r\n");
-		chprintf((BaseSequentialStream *)&SDU1, "%Ax=%-7d Ay=%-7d Az=%-7d Gx=%-7d Gy=%-7d Gz=%-7d\r\n\n", imu_values.acc_raw[0], imu_values.acc_raw[1], imu_values.acc_raw[2], imu_values.gyro_raw[0], imu_values.gyro_raw[1], imu_values.gyro_raw[2]);
-
+		// Reflect the orientation on the LEDs around the robot.
 		e_display_angle();
-
-		chThdSleepMilliseconds(100);
+		chThdSleepMilliseconds(50);
     }
 }
 
@@ -81,19 +101,46 @@ int main(void)
     /** Inits the Inter Process Communication bus. */
     messagebus_init(&bus, &bus_lock, &bus_condvar);
 
+    parameter_namespace_declare(&parameter_root, NULL, NULL);
+
     // Init the peripherals.
-    //motors_init();
-    imu_start(); 			// lance la thread qui s'occupe de l'update des mesures et de leur publication
-    serial_start();
-    usb_start();
+	clear_leds();
+	set_body_led(0);
+	set_front_led(0);
+	usb_start();
+	dcmi_start();
+	po8030_start();
+	motors_init();
+	proximity_start();
+	battery_level_start();
+	dac_start();
+	exti_start();
+	imu_start();
+	ir_remote_start();
+	spi_comm_start();
+	VL53L0X_start();
+	serial_start();
+	mic_start(NULL);
+	sdio_start();
+	playMelodyStart();
+	playSoundFileStart();
 
+	// Initialise Aseba system, declaring parameters
+    parameter_namespace_declare(&aseba_ns, &parameter_root, "aseba");
+    aseba_declare_parameters(&aseba_ns);
 
-    chThdCreateStatic(imu_thd_wa, sizeof(imu_thd_wa), NORMALPRIO, imu_thd, NULL);
+    /* Load parameter tree from flash. */
+    load_config();
 
+    /* Start AsebaCAN. Must be after config was loaded because the CAN id
+     * cannot be changed at runtime. */
+    aseba_vm_init();
+    aseba_can_start(&vmState);
+
+    chThdCreateStatic(selector_thd_wa, sizeof(selector_thd_wa), NORMALPRIO, selector_thd, NULL);
 
     /* Infinite loop. */
     while (1) {
-    	//waits 1 second
         chThdSleepMilliseconds(1000);
     }
 }
