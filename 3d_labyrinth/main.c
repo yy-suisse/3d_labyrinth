a7709288b45250d1fd7cf4c48d3f3e640aad841a
maure <romain.maure@epfl.ch>
2020-04-17 12:32:00 +0200

    deleted : useless stuff

--- 3d_labyrinth/main.c
+++ 3d_labyrinth/main.c
@@ -8,15 +8,9 @@
 #include "hal.h"
 #include "shell.h"
 
-#include "aseba_vm/aseba_node.h"
-#include "aseba_vm/skel_user.h"
-#include "aseba_vm/aseba_can_interface.h"
-#include "aseba_vm/aseba_bridge.h"
 #include "audio/audio_thread.h"
 #include "audio/play_melody.h"
-#include "audio/play_sound_file.h"
 #include "audio/microphone.h"
-#include "camera/po8030.h"
 #include "epuck1x/Asercom.h"
 #include "epuck1x/Asercom2.h"
 #include "epuck1x/a_d/advance_ad_scan/e_acc.h"
@@ -24,12 +18,11 @@
 #include "sensors/imu.h"
 #include "sensors/mpu9250.h"
 #include "sensors/proximity.h"
-#include "sensors/VL53L0X/VL53L0X.h"
+#include "ir_remote.h"
 #include "cmd.h"
 #include "config_flash_storage.h"
 #include "exti.h"
 #include "i2c_bus.h"
-#include "ir_remote.h"
 #include "leds.h"
 #include <main.h>
 #include "memory_protection.h"
@@ -57,29 +50,11 @@ messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
-parameter_namespace_t parameter_root, aseba_ns;
+parameter_namespace_t parameter_root;
 
 static THD_WORKING_AREA(controle_thd_wa, 2048);
 static THD_WORKING_AREA(prox_analyse_thd_wa, 2048);
 
-static bool load_config(void)
-{
-    extern uint32_t _config_start;
-
-    return config_load(&parameter_root, &_config_start);
-}
-
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
-
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
 
 bool get_controle_front(void)
 {
@@ -410,7 +385,6 @@ static THD_FUNCTION(controle_thd, arg)
 
 
     calibrate_acc();
-    calibrate_gyro();  	// VOIR SI NECESSAIRE //////////////////////////////////////////
 
     while(1)
     {
@@ -444,20 +418,15 @@ int main(void)
 	set_body_led(0);
 	set_front_led(0);
 	usb_start();
-	dcmi_start();
-	po8030_start();
 	motors_init();
 	proximity_start();
+	ir_remote_start();
 	battery_level_start();
 	dac_start();
 	exti_start();
-	ir_remote_start();
 	spi_comm_start();
-	VL53L0X_start();
-	serial_start();
 	sdio_start();
 	playMelodyStart();
-	playSoundFileStart();
 
 	if(get_selector()%2 == MODE_IMU)
 	{
@@ -471,18 +440,6 @@ int main(void)
 		pi_regulator_start();
 	}
 
-	// Initialise Aseba system, declaring parameters
-    parameter_namespace_declare(&aseba_ns, &parameter_root, "aseba");
-    aseba_declare_parameters(&aseba_ns);
-
-    /* Load parameter tree from flash. */
-    load_config();
-
-    /* Start AsebaCAN. Must be after config was loaded because the CAN id
-     * cannot be changed at runtime. */
-    aseba_vm_init();
-    aseba_can_start(&vmState);
-    calibrate_ir();
 
     chThdCreateStatic(prox_analyse_thd_wa, sizeof(prox_analyse_thd_wa), NORMALPRIO, prox_analyse_thd, NULL);
 
